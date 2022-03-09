2d2aea34a0139d5d643d889e82fec6a37d3440c0
foruser <vinhpham2761@gmail.com>
2022-03-10 00:57:25 +0700

    update

---
+++ src/main/java/com/laptrinhjavaweb/dao/implement/CategoryDAO.java
@@ -0,0 +1,73 @@
+package com.laptrinhjavaweb.dao.implement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.laptrinhjavaweb.dao.ICategoryDAO;
+import com.laptrinhjavaweb.model.Category;
+
+public class CategoryDAO implements ICategoryDAO {
+	
+	public Connection getConnection() {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String url = "jdbc:sqlserver://VINH\\\\SQLEXPRESS:1433;databaseName=PRJ301_Assignment";
+			String username = "sa";
+			String password = "123";
+			
+			return DriverManager.getConnection(url,username,password);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			return null;
+		} 
+		
+	}
+	
+	
+	@Override
+	public List<Category> getAll() {
+		List<Category> result = new ArrayList<>();
+		Connection connection = getConnection();
+		String sql = "select * from Category";
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		if (connection != null) {
+			try {
+				statement = connection.prepareStatement(sql);
+				resultSet = statement.executeQuery();
+				while (resultSet.next()) {
+					Category category = new Category();
+					category.setId(resultSet.getInt("id"));
+					category.setName(resultSet.getString("name"));
+					category.setCode(resultSet.getString("code"));
+					result.add(category);
+				}
+				
+				
+				
+				return result;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				return null;
+			}finally {
+				try {
+					connection.close();
+					statement.close();
+					resultSet.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					return null;
+				}
+			}
+			
+		}
+		return null;
+	}
+	
+	
+}
---
+++ src/main/java/com/laptrinhjavaweb/service/implement/CategoryService.java
@@ -0,0 +1,22 @@
+package com.laptrinhjavaweb.service.implement;
+
+import java.util.List;
+
+import com.laptrinhjavaweb.model.Category;
+import com.laptrinhjavaweb.service.ICategoryService;
+
+public class CategoryService implements ICategoryService{
+	
+	
+	
+	public CategoryService() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public List<Category> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
